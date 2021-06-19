8133038516eb4f900abdaa269ffd426f85291e41
DrewBeckley <atbeckley02@gmail.com>
2021-06-18 21:55:16 -0700

    first commit

---
+++ YahztzeeGui.py
@@ -0,0 +1,29 @@
+import random as R
+from tkinter import *
+YGui=Tk()
+##frame=Frame(top)
+YGui.title("Test 123")
+YGui.minsize(504,336)
+
+
+dice=Button(YGui,width=20,text="Submit Text",height=20)
+dice.place(x=100,y=100)
+def reroll():
+    roled=R.randint(1,7)
+    return roled
+def rerollSome(dices):
+    try:
+        #if type(dices) is list
+        dices.index(0)
+        raise TypeError("lists only") 
+    except ValueError:
+        #print("it doesn't have 0 being changed")
+        for diceRerolled in dices:
+            diceCurrent[diceRerolled]=reroll()
+#roll=R.randint(0,7)
+diceCurrent={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
+print(diceCurrent.values())
+#this will be replaced by gui 
+listtochange=[1,3,5]
+rerollSome(listtochange)
+#print(diceCurrent.values())
