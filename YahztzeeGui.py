66c718d9bd2109c440a9c3d45b4b0259cb423fee
Drew Beckley <85914430+DrewBeckley@users.noreply.github.com>
2021-06-20 06:56:34 +0000

    add reroll all and started test method to be a class and got gui with button to reroll all

--- YahztzeeGui.py
+++ YahztzeeGui.py
@@ -4,13 +4,16 @@ YGui=Tk()
 ##frame=Frame(top)
 YGui.title("Test 123")
 YGui.minsize(504,336)
-
-
-dice=Button(YGui,text="Submit Text")
-dice.place(x=100,y=100,height=20,width=100)
+diceCurrent={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
+dice=Button(YGui,text="Confirm", command=lambda:print(list(rerollAll().values())))
+dice.place(x=100,y=100,height=20,width=100) 
+def rerollAll():
+	for diceRerolled in diceCurrent:
+            diceCurrent[diceRerolled]=reroll()
+	return diceCurrent
+#I used this because it gives unque	
 def reroll():
-    roled=R.randint(1,7)
-    return roled
+    return R.randint(1,6)
 def rerollSome(dices):
     try:
         #if type(dices) is list
@@ -20,10 +23,17 @@ def rerollSome(dices):
         #print("it doesn't have 0 being changed")
         for diceRerolled in dices:
             diceCurrent[diceRerolled]=reroll()
-#roll=R.randint(0,7)
-diceCurrent={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
-print(diceCurrent.values())
-#this will be replaced by gui 
-listtochange=[1,3,5]
-rerollSome(listtochange)
-#print(diceCurrent.values())
+#tesing class
+#class:
+#
+#def __init__(self, name, age):
+    #self.name = name
+    #self.isin
+####
+def testing():
+	print(diceCurrent.values())
+	#this will be replaced by gui 
+	listtochange=[1,3,5]
+	rerollSome(listtochange)
+	print(diceCurrent.values())
+mainloop()
