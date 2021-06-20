f284ab79b18ad6b6098c75770a23a3c0b3909b09
Drew Beckley <85914430+DrewBeckley@users.noreply.github.com>
2021-06-20 20:06:51 +0000

    added mypy

--- Dice.py
+++ Dice.py
@@ -1,8 +1,9 @@
+import random as R
 class Dice:
-	def __init__(self,dicegroup,place):
-		self.num = reroll()
+	def __init__(self,place):
+		self.num = R.randint(1,6)
 		self.place=place
-		self.inorder=isinorder(place,dicegroup)
+		#self.inorder=isinorder(place,dicegroup)
 	def isinorder(self,position,dices):
 		if self.position != 1 and dices.get(self.position-1) is self.num-1:
 			print("me and the dice before are in order")
