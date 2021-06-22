4e68c2592b7572ab4e59bb794f8bb7f53b72eaf8
DrewBeckley <atbeckley02@gmail.com>
2021-06-22 16:30:14 -0700

    added a class for pure funtion methodsfor yahtzee and moved logic out of Gui class

---
+++ YahtzeeLogic.py
@@ -0,0 +1,54 @@
+class YahtzeeLogic:
+    #This class is for the logic of yahtzee game. I was thinking of pure functions and inmutablitity
+    #I will have 3 process method that will break it down in three ways:
+    """The first way will be a dicinary of each side rolled.
+This mean I can use this for the Yahtzee,upper section  of the score card, and x of a kind.
+The next is a sorted list to see if it is a x of a kind,yahtzee(by seeing if ends match)
+ordered list of sides for straights
+"""
+    def dictDictBreakDown(self,diceList):
+        #testing out may need to adjust later
+        dictVerson={i:diceList for i in range(0,len(diceList))}
+        return dictVersion
+    #need to do a mutation check to see if it changes the orginal lists
+    def sortedVersion(self,diceList):
+        return diceList.sort()
+    
+    def isFullHouse(rolls):
+        pass
+#         try:
+#             rolls.index(1)
+#             
+#         except valueError:
+    
+    #This could use small straight then
+    def isLargeStraight(self):
+        pass
+    def isSmallStraight(self):
+        pass
+    def chance(self):
+        ##sum up either using a build in function,reduce operation, or imperitive style
+        pass
+    def Breakdown(self):
+        #Ths is where a list  or dict could be returned for the gui. I plan to have a gui
+        #have all score boxes options and hilight the ones that are legal to pick for testing
+        pass
+        
+#logic part for rolling the dice/archive
+    def rerollAll(self):
+        for diceRerolled in self.diceList:
+            diceRerolled.reroll()
+        return self.diceList 
+
+    def DoSomething(self):
+        print(self.rerollAll())
+    def reroll(self):
+        return R.randint(1,6)
+    def rerollSome(self,dices):
+        try:
+            dices.index(0)
+            raise TypeError("lists only! you gave it a "+type(dices)) 
+        except ValueError:
+            for diceRerolled in dices:
+                diceRerolled.reroll()
+                return dices
--- YahztzeeGui.py
+++ YahztzeeGui.py
@@ -10,43 +10,23 @@ class pigPile:
     def DoSomething(self,event):
         print(self.rerollAll()) 
     dice=Button(YGui,text="Confirm")
-    #trying
-    xyz=lambda a:DoSomething()
+    #trying to add argument to bind 
+    xyz=lambda:DoSomething()
     dice.bind("<Button-1>",xyz)
     dice.place(x=10,y=10,height=20,width=100)
     
     
-    
-    def mypyTest(self,intiger:int)->None:
-        print(intiger)
     diceList=[]
     for n in range(4):
         x=D.Dice(n)
         diceList.append(x)
-#     mypyTest("This shouldn't work because of mypy(str !=int)")
-
-    #logic part/archive
-    def rerollAll(self):
-        for diceRerolled in self.diceList:
-            diceRerolled.reroll()
-        return self.diceList 
-
-    def DoSomething(self):
-        print(self.rerollAll())
-    #util vital could go into its own class
-    def rerollSome(self,dices):
-        try:
-            dices.index(0)
-            raise TypeError("lists only! you gave it a "+type(dices)) 
-        except ValueError:
-            for diceRerolled in dices:#diceCurrent[diceRerolled]=reroll()
-                diceRerolled.reroll()
-
-
-
-    #just to keep the interpriter from complaining
-    def reroll(self):
-        return R.randint(1,6)
+#     for _ in range(4):
+#         dicelist=dicelist.
+        
+    #thinking about mypy for static
+    def mypyTest(self,intiger:int)->None:
+        print(intiger)
+#    mypyTest("This shouldn't work because of mypy(str !=int)")
     #diceCurrenit={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
     YGui.mainloop()
     
