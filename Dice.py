75300d459ff91730b9d1e0de345ebe8fb3c951cd
DrewBeckley <atbeckley02@gmail.com>
2021-06-21 20:44:54 -0700

    fixed some undefined variables

--- Dice.py
+++ Dice.py
@@ -1,13 +1,14 @@
 import random as R
 class Dice:
-	def __init__(self,place):
-		self.num = R.randint(1,6)
-		self.place=place
-		#self.inorder=isinorder(place,dicegroup)
-	def isinorder(self,position,dices):
-		if self.position != 1 and dices.get(self.position-1) is self.num-1:
-			print("me and the dice before are in order")
-		#dice.get()
-	#I used this because it gives unique every time	
-	def reroll():
-    		self.num=R.randint(1,6)
+    def __init__(self,place):
+        self.num = R.randint(1,6)
+        self.place=place
+        #self.inorder=isinorder(place,dicegroup)
+    def isinorder(self,position,dices):
+        if self.position != 1 and dices.get(self.position-1) is self.num-1:
+            print("me and the dice before are in order")
+        #dice.get()
+    #I used this because it gives unique every time
+    def reroll(self):
+        self.num=R.randint(1,6)
+
--- YahztzeeGui.py
+++ YahztzeeGui.py
@@ -3,41 +3,49 @@ from tkinter import *
 import Dice as D
 #I chosse this name becuse it
 class pigPile:
-	YGui=Tk()
-	YGui.title("Yahtzee")
-	YGui.minsize(504,336)
-	mypyTest("This shouldn't work because of mypy(str !=int)")
-	dice=Button(YGui,text="Confirm", command=confirmEntry)
-	dice.place(x=10,y=10,height=20,width=100) 
-	def mypyTest(self,intiger:int)->None:
-		print(intiger)
-	diceList=[]
-	for n in range(4):
-		x=D.Dice(n)
-		diceList.append(x)
+    YGui=Tk()
+    YGui.title("Yahtzee")
+    YGui.minsize(504,336)
+    #mypyTest("This shouldn't work because of mypy(str !=int)")
+    def DoSomething(self,event):
+        print(self.rerollAll()) 
+    dice=Button(YGui,text="Confirm")
+    xyz=DoSomething
+    dice.bind("<Button-1>",xyz)
+    dice.place(x=10,y=10,height=20,width=100)
+    
+    
+    
+    def mypyTest(self,intiger:int)->None:
+        print(intiger)
+    diceList=[]
+    for n in range(4):
+        x=D.Dice(n)
+        diceList.append(x)
+#     mypyTest("This shouldn't work because of mypy(str !=int)")
+
+    #logic part/archive
+    def rerollAll(self):
+        for diceRerolled in self.diceList:
+            diceRerolled.reroll()
+        return self.diceList 
+
+    def DoSomething(self):
+        print(self.rerollAll())
+    #util vital could go into its own class
+    def rerollSome(self,dices):
+        try:
+            dices.index(0)
+            raise TypeError("lists only! you gave it a "+type(dices)) 
+        except ValueError:
+            for diceRerolled in dices:#diceCurrent[diceRerolled]=reroll()
+                diceRerolled.reroll()
 
 
-	#logic part/archive
-	def rerollAll(self):
-		for diceRerolled in diceList:
-			diceRerolled.reroll()
-		return diceList 
 
-	def confirmEntry(self):
-		print(self.rerollAll())
-	#util vital could go into its own class
-	def rerollSome(self,dices):
-		try:
-			dices.index(0)
-			raise TypeError("lists only! you gave it a "+type(dices)) 
-		except ValueError:
-			for diceRerolled in dices:#diceCurrent[diceRerolled]=reroll()
-				diceRerolled.reroll()
-
-
-
-	#just to keep the interpriter from complaining
-	def reroll(self):
-		return R.randint(1,6)
-	#diceCurrenit={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
-	mainloop()
+    #just to keep the interpriter from complaining
+    def reroll(self):
+        return R.randint(1,6)
+    #diceCurrenit={1:reroll(),2:reroll(), 3:reroll(),4:reroll(),5:reroll()}
+    YGui.mainloop()
+    
